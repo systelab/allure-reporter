95107da5ca0416ecac1426a7eebb7805daf7a0ab
Alfons Serra <alfonsoserra@gmail.com>
2018-04-12 19:03:44 +0200

    Delete deploy.sh

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-if [ -n "$GITHUB_API_KEY" ]; then
-    git clone https://github.com/systelab/systelab.github.io.git
-    cp ./dist/* ./systelab.github.io/allure-reporter
-    cd systelab.github.io
-    git add .
-    git -c user.name='travis' -c user.email='travis' commit -m update
-    git push -f -q https://systelab:$GITHUB_API_KEY@github.com/systelab/systelab.github.io
-  fi
