2ea3c9e8edbdbda74fa69c8b67f19c564d711aab
Alfons Serra <alfonsoserra@gmail.com>
2018-04-12 18:49:29 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,8 @@
 if [ -n "$GITHUB_API_KEY" ]; then
-    cd "$TRAVIS_BUILD_DIR"
     git clone https://github.com/systelab/systelab.github.io.git
     cp ./dist/* ./systelab.github.io/allure-reporter
     cd systelab.github.io
     git add .
     git -c user.name='travis' -c user.email='travis' commit -m update
-    # Make sure to make the output quiet, or else the API token will leak!
-    # This works because the API key can replace your password.
-    git push -f -q https://systelab:$GITHUB_API_KEY@github.com/systelab/systelab.github.io &2>/dev/null
+    git push -f -q https://systelab:$GITHUB_API_KEY@github.com/systelab/systelab.github.io
   fi
