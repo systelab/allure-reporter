5e9f56e159d5bd9a77e649bad80050e57ec180fe
alfonsserra <alfonsoserra@gmail.com>
2018-04-16 17:51:01 +0200

    Add Create Test Cycle Method

--- src/app/common/report/report.component.ts
+++ src/app/common/report/report.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ProjectsService } from '../../jama';
+import { ProjectsService, RequestTestCycle } from '../../jama';
+import { TestplansService } from '../../jama/api/testplans.service';
 
 @Component({
 	selector:    'app-report',
@@ -13,7 +14,13 @@ export class ReportComponent {
 
 	@Output() public close = new EventEmitter();
 
-	constructor(private projectsService: ProjectsService) {
+	public project = '';
+	public testplan = '';
+	public testcycle = '';
+	public reporter = '';
+	public testgroups = '';
+
+	constructor(private projectsService: ProjectsService, private testplansService: TestplansService) {
 	}
 
 	public doClose() {
@@ -24,11 +31,44 @@ export class ReportComponent {
 		this.projectsService.configuration.username = this.username;
 		this.projectsService.configuration.password = this.password;
 		this.projectsService.configuration.basePath = this.server;
-		this.projectsService.getProject(30)
+		this.projectsService.getProject(Number(this.project))
 			.subscribe(
 				(wrapper) => {
 					console.log(wrapper.data);
+					const testGroupsToInclude: Array<number> = [];
+
+					const res = this.testgroups.split(',');
+					for (let i = 0; i < res.length; i++) {
+						testGroupsToInclude.push(Number(res[i].trim()));
+					}
+					this.createTestCycle(Number(this.project), Number(this.testplan), this.testcycle, testGroupsToInclude);
 				}
 			)
+	}
+
+	private createTestCycle(project: number, testplan: number, testCycleName: string, testGroupsToInclude: Array<number>) {
+		this.testplansService.configuration.username = this.username;
+		this.testplansService.configuration.password = this.password;
+		this.testplansService.configuration.basePath = this.server;
+
+		const requestTestCycle: RequestTestCycle = {
+			'fields':                  {
+				'name':      testCycleName,
+				'project':   project,
+				'startDate': '2018-01-01',
+				'endDate':   '2018-01-15'
+			},
+			'testRunGenerationConfig': {
+				'testGroupsToInclude':      testGroupsToInclude,
+				'testRunStatusesToInclude': ['PASSED', 'NOT_RUN', 'INPROGRESS', 'FAILED', 'BLOCKED']
+			}
+		};
+
+		this.testplansService.createTestCycle(requestTestCycle, testplan)
+			.subscribe(
+				(createdResponse) => {
+					console.log(createdResponse)
+				}
+			);
 	}
 }
